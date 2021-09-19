8da7e4a13b61052046b1a199fe46605febb6980f
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-09-19 11:02:58 +0200

    adding a client subproject instead of one big CMakeLists.txt

---
+++ Client/src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by cbihan on 19/09/2021.
+//
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello from client" << std::endl;
+}
---
+++ Client/tests/Catch2Main.cpp
@@ -0,0 +1,6 @@
+//
+// Created by cbihan on 18/09/2021.
+//
+
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
