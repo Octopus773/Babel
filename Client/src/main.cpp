28439b5414696fa32ec1112b0b5308d77a2672e4
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-09-24 14:19:54 +0200

    disabling qt in Client/main.cpp

--- Client/src/main.cpp
+++ Client/src/main.cpp
@@ -5,12 +5,12 @@
 #include <iostream>
 #include <portaudio.h>
 #include <opus/opus.h>
-#include <qt5/QtCore/QtCoreVersion>
+//#include <qt5/QtCore/QtCoreVersion>
 
 int main()
 {
 	std::cout << "Hello from client" << std::endl;
 	std::cout << "pulseaudio version: " << Pa_GetVersion() << std::endl;
 	std::cout << "opus version " << opus_get_version_string() << std::endl;
-	std::cout << "qT version " << QTCORE_VERSION_STR << " : " << QTCORE_VERSION << std::endl;
+	//std::cout << "qT version " << QTCORE_VERSION_STR << " : " << QTCORE_VERSION << std::endl;
 }
