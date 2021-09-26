f74b418d29f7aa73993e90afd219abd80bab06a1
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-09-26 10:32:25 +0200

    changing the exemple for asio

--- Server/src/main.cpp
+++ Server/src/main.cpp
@@ -2,76 +2,218 @@
 // Created by cbihan on 19/09/2021.
 //
 
+#include <cstdlib>
+#include <deque>
 #include <iostream>
-#include "Exceptions/BabelException.hpp"
-
-#include <asio/co_spawn.hpp>
+#include <list>
+#include <memory>
+#include <set>
+#include <string>
+#include <utility>
+#include <asio/awaitable.hpp>
 #include <asio/detached.hpp>
+#include <asio/co_spawn.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/tcp.hpp>
+#include <asio/read_until.hpp>
+#include <asio/redirect_error.hpp>
 #include <asio/signal_set.hpp>
+#include <asio/steady_timer.hpp>
+#include <asio/use_awaitable.hpp>
 #include <asio/write.hpp>
-#include <cstdio>
 
 using asio::ip::tcp;
 using asio::awaitable;
 using asio::co_spawn;
 using asio::detached;
+using asio::redirect_error;
 using asio::use_awaitable;
-namespace this_coro = asio::this_coro;
 
-#if defined(ASIO_ENABLE_HANDLER_TRACKING)
-# define use_awaitable \
-  asio::use_awaitable_t(__FILE__, __LINE__, __PRETTY_FUNCTION__)
-#endif
+//----------------------------------------------------------------------
 
-awaitable<void> echo(tcp::socket socket)
+class chat_participant
 {
-	try
+public:
+	virtual ~chat_participant() {}
+	virtual void deliver(const std::string& msg) = 0;
+};
+
+typedef std::shared_ptr<chat_participant> chat_participant_ptr;
+
+//----------------------------------------------------------------------
+
+class chat_room
+{
+public:
+	void join(chat_participant_ptr participant)
 	{
-		char data[1024];
-		for (;;)
+		participants_.insert(participant);
+		for (auto msg: recent_msgs_)
+			participant->deliver(msg);
+	}
+
+	void leave(chat_participant_ptr participant)
+	{
+		participants_.erase(participant);
+	}
+
+	void deliver(const std::string& msg)
+	{
+		recent_msgs_.push_back(msg);
+		while (recent_msgs_.size() > max_recent_msgs)
+			recent_msgs_.pop_front();
+
+		for (auto participant: participants_)
+			participant->deliver(msg);
+	}
+
+private:
+	std::set<chat_participant_ptr> participants_;
+	enum { max_recent_msgs = 100 };
+	std::deque<std::string> recent_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+class chat_session
+	: public chat_participant,
+	  public std::enable_shared_from_this<chat_session>
+{
+public:
+	chat_session(tcp::socket socket, chat_room& room)
+		: socket_(std::move(socket)),
+		  timer_(socket_.get_executor()),
+		  room_(room)
+	{
+		timer_.expires_at(std::chrono::steady_clock::time_point::max());
+	}
+
+	void start()
+	{
+		room_.join(shared_from_this());
+
+		co_spawn(socket_.get_executor(),
+		         [self = shared_from_this()]{ return self->reader(); },
+		         detached);
+
+		co_spawn(socket_.get_executor(),
+		         [self = shared_from_this()]{ return self->writer(); },
+		         detached);
+	}
+
+	void deliver(const std::string& msg)
+	{
+		write_msgs_.push_back(msg);
+		timer_.cancel_one();
+	}
+
+private:
+	awaitable<void> reader()
+	{
+		try
 		{
-			std::size_t n = co_await socket.async_read_some(asio::buffer(data), use_awaitable);
-			co_await async_write(socket, asio::buffer(data, n), use_awaitable);
+			for (std::string read_msg;;)
+			{
+				std::size_t n = co_await asio::async_read_until(socket_,
+				                                                asio::dynamic_buffer(read_msg, 1024), "\n", use_awaitable);
+
+				room_.deliver(read_msg.substr(0, n));
+				read_msg.erase(0, n);
+			}
+		}
+		catch (std::exception&)
+		{
+			stop();
 		}
 	}
-	catch (std::exception& e)
-	{
-		std::printf("echo Exception: %s\n", e.what());
-	}
-}
 
-awaitable<void> listener()
+	awaitable<void> writer()
+	{
+		try
+		{
+			while (socket_.is_open())
+			{
+				if (write_msgs_.empty())
+				{
+					asio::error_code ec;
+					co_await timer_.async_wait(redirect_error(use_awaitable, ec));
+				}
+				else
+				{
+					co_await asio::async_write(socket_,
+					                           asio::buffer(write_msgs_.front()), use_awaitable);
+					write_msgs_.pop_front();
+				}
+			}
+		}
+		catch (std::exception&)
+		{
+			stop();
+		}
+	}
+
+	void stop()
+	{
+		room_.leave(shared_from_this());
+		socket_.close();
+		timer_.cancel();
+	}
+
+	tcp::socket socket_;
+	asio::steady_timer timer_;
+	chat_room& room_;
+	std::deque<std::string> write_msgs_;
+};
+
+//----------------------------------------------------------------------
+
+awaitable<void> listener(tcp::acceptor acceptor)
 {
-	auto executor = co_await this_coro::executor;
-	tcp::acceptor acceptor(executor, {tcp::v4(), 55555});
+	chat_room room;
+
 	for (;;)
 	{
-		tcp::socket socket = co_await acceptor.async_accept(use_awaitable);
-		co_spawn(executor, echo(std::move(socket)), detached);
+		std::make_shared<chat_session>(
+			co_await acceptor.async_accept(use_awaitable),
+			room
+		)->start();
 	}
 }
 
-int main()
+//----------------------------------------------------------------------
+
+int main(int argc, char* argv[])
 {
 	try
 	{
+		if (argc < 2)
+		{
+			std::cerr << "Usage: chat_server <port> [<port> ...]\n";
+			return 1;
+		}
+
 		asio::io_context io_context(1);
+
+		for (int i = 1; i < argc; ++i)
+		{
+			unsigned short port = std::atoi(argv[i]);
+			co_spawn(io_context,
+			         listener(tcp::acceptor(io_context, {tcp::v4(), port})),
+			         detached);
+		}
 
 		asio::signal_set signals(io_context, SIGINT, SIGTERM);
 		signals.async_wait([&](auto, auto){ io_context.stop(); });
-
-		co_spawn(io_context, listener(), detached);
 
 		io_context.run();
 	}
 	catch (std::exception& e)
 	{
-		std::printf("Exception: %s\n", e.what());
+		std::cerr << "Exception: " << e.what() << "\n";
 	}
-}
 
+	return 0;
+}
 /*
 int main()
 {
