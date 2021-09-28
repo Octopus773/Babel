40559b6a8353ae3614f091d83dbb9f65ccac6066
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-09-28 16:43:17 +0200

    adding V0.1 of messages

--- Common/Network/ITCP.hpp
+++ Common/Network/ITCP.hpp
@@ -3,3 +3,17 @@
 //
 
 #pragma once
+
+#include <string>
+
+namespace Babel
+{
+	class ITCP
+	{
+		void connect(const std::string &hostname, const std::string &port);
+		void disconnect();
+		bool isConnected();
+		void send();
+
+	};
+}
