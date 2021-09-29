4a9891ece5b275a2b67f1e65bfa565598030dea2
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-09-29 15:57:45 +0200

    Specifying ITCP interface into a client specific interface and addressing some issues (TSQueue)

--- Common/Network/IUDPClient.hpp
+++ Common/Network/IUDPClient.hpp
@@ -8,13 +8,11 @@
 
 namespace Babel
 {
-	class ITCP
+	class IUDPClient
 	{
 	public:
 		//! @brief connect to a hostname and a port
-		void connect(const std::string &hostname, const std::string &port) = 0;
-		//! @brief disconnect the connection
-		void disconnect() = 0;
+		void connect(const std::string &hostname, uint16_t port) = 0;
 		//! @brief Send the message
 		void send(Message message) = 0;
 	};
