58b859e15b1012a70c0ec7a4791b0d96f69fecac
Djadanis <50323196+Djadanis@users.noreply.github.com>
2021-09-29 11:04:50 +0200

    Added Doxygen doc as well as fixed the case

--- Common/Utilities/TSQueue.hpp
+++ Common/Utilities/TSQueue.hpp
@@ -17,21 +17,24 @@ namespace Babel
 	public:
 		TSQueue() = default;
 		TSQueue(const TSQueue<T>&) = delete;
-		~TSQueue() { clear(); };
+		~TSQueue() { this->clear(); };
 
+		//!	@brief Returns a constant reference to the first element of the queue
 		const T& front()
 		{
 			std::scoped_lock lock(_mutex);
 			return _queue.front();
 		}
 
+		//!	@brief Returns a constant reference to the last element of the queue
 		const T& back()
 		{
 			std::scoped_lock lock(_mutex);
 			return _queue.back();
 		}
 
-		T pop_front()
+		//!	@brief Removes and returns the first element of the queue
+		T popFront()
 		{
 			std::scoped_lock lock(_mutex);
 			auto t = std::move(_queue.front());
@@ -39,7 +42,8 @@ namespace Babel
 			return t;
 		}
 
-		T pop_back()
+		//!	@brief Removes and returns the last element of the queue
+		T popBack()
 		{
 			std::scoped_lock lock(_mutex);
 			auto t = std::move(_queue.back());
@@ -47,30 +51,35 @@ namespace Babel
 			return t;
 		}
 
-		void push_front(const T& t)
+		//!	@brief Adds an element to the beginning of the queue
+		void pushFront(const T& t)
 		{
 			std::scoped_lock lock(_mutex);
 			_queue.push_front(t);
 		}
 
-		void push_back(const T& t)
+		//!	@brief Adds an element to the beginning of the queue
+		void pushBack(const T& t)
 		{
 			std::scoped_lock lock(_mutex);
 			_queue.push_back(t);
 		}
 
+		//!	@brief Returns true if the queue is empty, false otherwise
 		bool empty()
 		{
 			std::scoped_lock lock(_mutex);
 			return _queue.empty();
 		}
 
+		//!	@brief Returns the number of elements in the queue
 		size_t count()
 		{
 			std::scoped_lock lock(_mutex);
 			return _queue.size();
 		}
 
+		//!	@brief Removes every elements in the queue
 		void clear()
 		{
 			std::scoped_lock lock(_mutex);
