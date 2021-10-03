5475bc59b30e5cdfb537fdaa4d338dca06175bb9
ugenereau <ugo.genereau@epitech.eu>
2021-10-03 17:02:41 +0200

    added  portaudio encapsulation

---
+++ Common/Audio/IAudioManager.hpp
@@ -0,0 +1,52 @@
+/*
+** EPITECH PROJECT, 2021
+** IAudioManager.hpp
+** File description:
+** Abstraction class of a Audio library
+*/
+
+#ifndef IAUDIOMANAGER_HPP_
+#define IAUDIOMANAGER_HPP_
+#include <vector>
+#include <cstdint>
+
+namespace Babel
+{
+    //! @brief Audio Module Interface
+    class IAudioManager
+    {
+    public:
+        //! @brief set the number of channels for future streams
+        virtual void setChannelsNumber(int32_t) = 0;
+        //! @brief get the number of channels for future streams
+        virtual int32_t getChannelsNumber() const = 0;
+        //! @brief set the samplerate for future streams
+        virtual void setSampleRate(int32_t) = 0;
+        //! @brief get the current samplerate
+        virtual int32_t getSampleRate() const = 0;
+        //! @brief get the record time for readStreams
+        virtual int32_t getRecordTime() const = 0;
+        //! @brief get the record time for readStreams
+        virtual void setRecordTime(int32_t) = 0;
+        //! @brief get the frame per buffer value for readStreams
+        virtual int32_t getFramesPerBuffer() const = 0;
+        //! @brief get the frame per buffer for readStreams
+        virtual void setFramesPerBuffer(int32_t) = 0;
+        //! @brief Open stream to input and output sounds
+        virtual void openStream() = 0;
+        //! @brief Start a stream
+        virtual void startStream() = 0;
+        //! @brief Stop a stream
+        virtual void stopStream() = 0;
+        //! @brief Close current stream
+        virtual void closeStream() = 0;
+        //! @brief Read an array of int16_t from current stream
+        virtual std::vector<int16_t> readStream() = 0;
+        //! @brief Write an array of int16_t to current stream
+        virtual void writeStream(std::vector<int16_t> &) = 0;
+        //! @brief virtual destructor
+        virtual ~IAudioManager() = default;
+    };
+};
+
+#endif
