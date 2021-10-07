320914ffa02432a07190072fdedc1b227ecef34f
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-10-07 10:29:56 +0200

    changing every exception to babel exceptions

--- Client/src/Audio/PortAudio.cpp
+++ Client/src/Audio/PortAudio.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "PortAudio.hpp"
+#include "Exceptions/BabelException.hpp"
 #include <iostream>
 #include <exception>
 
@@ -13,15 +14,14 @@ Babel::PortAudio::PortAudio()
 {
 	int err = Pa_Initialize();
 	this->_streamStopped = true;
-	this->_stream = NULL;
+	this->_stream = nullptr;
 	this->_frames_per_buffer = 960;
 	this->_samplerate = 48000;
 	this->_number_channels = 2;
 	this->_recordtime = 10;
 
 	if (err != paNoError) {
-		std::cerr << "Error in intializing PortAudio" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error in intializing PortAudio");
 	}
 }
 
@@ -73,8 +73,7 @@ void Babel::PortAudio::openStream()
 
 	inputParameters.device = Pa_GetDefaultInputDevice();
 	if (inputParameters.device == paNoDevice) {
-		std::cerr << "No imput devices found" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("No imput devices found");
 	}
 	inputParameters.channelCount = this->_number_channels;
 	inputParameters.sampleFormat = PA_SAMPLE_TYPE;
@@ -82,19 +81,17 @@ void Babel::PortAudio::openStream()
 	inputParameters.hostApiSpecificStreamInfo = nullptr;
 	outputParameters.device = Pa_GetDefaultOutputDevice();
 	if (outputParameters.device == paNoDevice) {
-		std::cerr << "No default output device found" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("No default output device found");
 	}
 	outputParameters.channelCount = this->_number_channels;
 	outputParameters.sampleFormat = PA_SAMPLE_TYPE;
 	outputParameters.suggestedLatency = Pa_GetDeviceInfo(outputParameters.device)->defaultLowOutputLatency;
 	outputParameters.hostApiSpecificStreamInfo = nullptr;
-	err = Pa_OpenStream(&this->_stream, &inputParameters, &outputParameters, this->_samplerate, this->_frames_per_buffer, paClipOff, NULL,
-	                    NULL);
+	err = Pa_OpenStream(&this->_stream, &inputParameters, &outputParameters, this->_samplerate, this->_frames_per_buffer, paClipOff, nullptr,
+	                    nullptr);
 	if (err != paNoError || !this->_stream) {
 		this->_stream = nullptr;
-		std::cerr << "Could not open output stream" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Could not open output stream");
 	}
 }
 
@@ -103,8 +100,7 @@ void Babel::PortAudio::startStream()
 	int32_t err = paNoError;
 
 	if (!this->_stream) {
-		std::cerr << "Audio stream not set" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Audio stream not set");
 	}
 	if (!this->_streamStopped) {
 		std::cerr << "Audio stream already started" << std::endl;
@@ -112,8 +108,7 @@ void Babel::PortAudio::startStream()
 	}
 	err = Pa_StartStream(this->_stream);
 	if (err != paNoError) {
-		std::cerr << "Error: could not start audio stream" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: could not start audio stream");
 	}
 	this->_streamStopped = false;
 }
@@ -151,18 +146,15 @@ std::vector<int16_t> Babel::PortAudio::readStream()
 	int32_t err = paNoError;
 
 	if (!this->_stream) {
-		std::cerr << "Error: stream is not set" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: stream is not set");
 	}
 	if (this->_streamStopped) {
-		std::cerr << "Error: stream not started" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: stream not started");
 	}
 	std::fill(data.begin(), data.end(), 0);
 	err = Pa_ReadStream(this->_stream, data.data(), this->_frames_per_buffer);
 	if (err != paNoError) {
-		std::cerr << "Error: could not read from stream" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: could not read from stream");
 	}
 	return (data);
 }
@@ -172,17 +164,14 @@ void Babel::PortAudio::writeStream(std::vector<int16_t> &data)
 	int32_t err = paNoError;
 
 	if (!this->_stream) {
-		std::cerr << "Error: stream is not set" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: stream is not set");
 	}
 	if (this->_streamStopped) {
-		std::cerr << "Error: stream not started" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: stream not started");
 	}
 	err = Pa_WriteStream(this->_stream, data.data(), data.size() / this->_number_channels);
 	if (err != paNoError) {
-		std::cerr << "Error: could not write to stream" << std::endl;
-		throw std::exception();
+		throw Exception::BabelException("Error: could not write to stream");
 	}
 }
 
