0ab1e842583e4e400202173cd8f444a70ccfe794
ugenereau <ugo.genereau@epitech.eu>
2021-10-07 09:25:58 +0200

    removed useless files made by cmake

--- Client/CMakeFiles/babel_client_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/babel_client_autogen"
-  "babel_client_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/babel_client_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- Client/CMakeFiles/client_unit_tests_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/client_unit_tests_autogen"
-  "client_unit_tests_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/client_unit_tests_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
