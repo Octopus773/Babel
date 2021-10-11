5e75e7a59c19c09492b6a2b70304a54d80183979
mgl <marius.glo@protonmail.com>
2021-10-11 18:14:54 +0200

    Added Doxygen

--- Client/src/Network/UDPSocket.hpp
+++ Client/src/Network/UDPSocket.hpp
@@ -8,17 +8,23 @@
 #include <QUdpSocket>
 
 namespace Babel {
+    //! @class wrappers around Qt UDP socket
     class UDPSocket : public QObject {
     Q_OBJECT
     public:
+        //! @brief ctor
         UDPSocket(const QHostAddress &address, std::int16_t port);
 
+        //! @brief dtor
         ~UDPSocket() override;
 
+        //! @brief reads the incoming datagrams when they arrive
         void readPending();
 
+        //! @brief reads maxSize from socket and writes to data
         std::int64_t read(char *data, std::int64_t maxSize);
 
+        //! @brief writes data to socket
         std::int64_t write(char *data, std::int64_t size);
 
     private:
